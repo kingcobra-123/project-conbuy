1460baa49baff359361bce53c3041b7068b2c666
kingcobra-123 <satish.kokkanti96@gmail.com>
2024-06-05 18:25:17 -0700

    updated env variables

--- server/index.js
+++ server/index.js
@@ -58,19 +58,20 @@ app.use("/posts", postRoutes);
 
 // Mongoose setup
 
-const PORT = process.env.PORT || 3500;
-mongoose.connect(process.env.MONGO_URL)
-.then(() => {
+const PORT = process.env.PORT || 3000;
+mongoose
+  .connect(process.env.AZURE_COSMOS_CONNECTIONSTRING)
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
 
-        // to load test data - RUN ONLY ONCE!!!
-        // User.insertMany(users);
-        // Post.insertMany(posts);
-        // Category.insertMany(categories);
+      // to load test data - RUN ONLY ONCE!!!
+      // User.insertMany(users);
+      // Post.insertMany(posts);
+      // Category.insertMany(categories);
     });
-    console.log('Database connected');
-}).catch((err) => {
-    console.log('Database connection error: ', err);
-}
-);
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log("Database connection error: ", err);
+  });
